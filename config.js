d4bfc04e7b9ff86d158bd2b6ef623cee6aa476c7
DominicDev <s0l0estudi4r@gmail.com>
2025-12-18 13:28:43 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MARRACASH ';
+export const nameSongs = [
+	"∞ LOVE", /*1*/
+	"NIENTE CANZONI D'AMORE", /*2*/
+	"CRUDELIA", /*3*/
+	"IN RADIO", /*4*/
+	"CRAZY LOVE", /*5*/
+	"MADAME", /*6*/
+	"A VOLTE ESAGERO", /*7*/
+	"BRAVI A CADERE", /*8*/
+	"S.E.N.I.C.A.R.", /*9*/
+	"ELISA", /*10*/
+	"KING DEL RAP", /*11*/
+	"SUPREME", /*12*/
+	"IMPORTANTE", /*13*/
+	"BADABUM", /*14*/
+	"DUBBI", /*15*/
+	"LAUREA", /*16*/
+	"NEMESI", /*17*/
+	"GLI SBANDATI HANNO PERSO", /*18*/
+	"NOI", /*19*/
+	"VENDETTA", /*20*/
+]
